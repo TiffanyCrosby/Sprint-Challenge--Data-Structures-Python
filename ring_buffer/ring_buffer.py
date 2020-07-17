74266170e4fb4634a4e049b22afa824ad734fc75
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-07-17 11:47:48 -0400

    ring-buffer working. tests passing. had to change imports in test file. it wasn't importing RingBuffer.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity, data=[]):
+        self.list_index = 0 #makes it so i can replace new item with old item starting at first item
+        self.capacity = capacity
+        self.data = list(data)[capacity:] #using list to store items
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity: #check to see if list is full
+            self.data[self.list_index] = item #if list is full then replace OLDEST item with item passed in
+        else:
+            self.data.append(item) #adding item to list 
+
+        self.list_index = (self.list_index + 1) % self.capacity #increments index in order to replace oldest item NOT first item,
+        # also checks to make sure not going past capacity
 
     def get(self):
-        pass
+        return self.data #returns list
--- ring_buffer/test_ring_buffer.py
+++ ring_buffer/test_ring_buffer.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+sys.path[:0] = ['ring_buffer']
 from ring_buffer import RingBuffer
 
 class RingBufferTests(unittest.TestCase):
